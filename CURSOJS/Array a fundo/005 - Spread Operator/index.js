d8da48e58b8fe859aafda3ee7479d43cfb3cf5f3
Gustavo <122311037+GUTOFAR1AS@users.noreply.github.com>
2024-07-30 03:49:03 -0300

    Add files via upload

---
+++ CURSOJS/Array a fundo/005 - Spread Operator/index.js
@@ -0,0 +1,6 @@
+const num = [1, 2, 3, 4, 5]
+
+console.log(...num);   // ... TIRA OS ARRAYS    
+console.log(Math.max(1, 2, 3, 4, 5));
+
+console.log(Math.max(...num))
---
+++ CURSOJS/Array a fundo/007 - Removendo Elementos/index.js
@@ -0,0 +1,12 @@
+/*
+    shift = remove o primeiro elemento de um array e retorna esse elemento 
+    pop = remove o último elemento de um array e retorna este elemento 
+*/
+
+let arr = [1, 2, 3, 4, 5]
+
+console.table(arr);
+
+arr.shift();
+
+console.log(arr);
